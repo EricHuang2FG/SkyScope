bf548cba94adc69bf183ceb41b0ff159d1c7a7b6
laygofiona <laygo.fiona@gmail.com>
2024-05-18 02:54:24 -0400

    adding buttons to the card

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -5,6 +5,10 @@ function Card({ celestialBody }) {
         <div className="card">
             <img src={celestialBody.image} />
             <h3>{celestialBody.name}</h3>
+            <div className="overlay">
+                <button id="tracking-button">Track Me</button>
+                <button id="learning-button">Learn More</button>
+            </div>
         </div>
     )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
         <div className="nav-bar">
             <h1>SkyScope</h1>
             <div className="search-bar">
-                <input type="text"  onKeyDown={console.log("working")}  id="search" name="search" onChange={console.log("working")} value={""} placeholder="Search over 100+ celestial bodies: planets, moons, satellites, etc..." aria-label="Search bar" required/>
+                <input type="text"   id="search" name="search" placeholder="Search over 100+ celestial bodies: planets, moons, satellites, etc..." aria-label="Search bar" required/>
                 <button type="submit" className="search-button" onClick={console.log("working")}>
                     <i className="fa fa-search"></i>
                 </button>
