aa58f796342cd4904e2c379c40105a3941337dfc
Eric Huang <xilahuade@foxmail.com>
2024-05-18 10:08:12 -0400

    Added vex template

---
+++ vex/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       rui                                                          #
+# 	Created:      5/18/2024, 10:03:54 AM                                       #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        
