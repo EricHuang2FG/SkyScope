a131952361a4408cb386c9be80f5ea0d8974abe6
Eric Huang <xilahuade@foxmail.com>
2024-05-18 21:14:24 -0400

    Working servergit add .git add .

---
+++ frontend/compass.js
@@ -0,0 +1,74 @@
+let server = `localhost`
+let heading = 0;
+let pitch = 0;
+
+const run = () => {
+  if ("AbsoluteOrientationSensor" in window) {
+    let compass = new AbsoluteOrientationSensor();
+    compass.addEventListener("reading", function (e) {
+      let q = e.target.quaternion;
+
+      console.log(e.target);
+
+      heading =
+        Math.atan2(
+          2 * q[0] * q[1] + 2 * q[2] * q[3],
+          1 - 2 * q[1] * q[1] - 2 * q[2] * q[2]
+        ) *
+        (180 / Math.PI);
+    });
+
+    compass.start();
+
+    console.log("compass created");
+  } else {
+    alert("not supported");
+  }
+
+  if ("Accelerometer" in window) {
+    let accelerometer = new Accelerometer();
+    accelerometer.addEventListener("reading", function (e) {
+      pitch = e.target.y;
+
+      document.innerHTML = pitch;
+    });
+
+    accelerometer.start();
+
+    console.log("accelerometer created");
+  } else {
+    alert("not supported");
+  }
+};
+
+const setup = async () => {
+  server = prompt("server");
+ 
+  const permissions = await Promise.all([
+    navigator.permissions.query({ name: "accelerometer" }),
+    navigator.permissions.query({ name: "magnetometer" }),
+    navigator.permissions.query({ name: "gyroscope" }),
+  ]);
+
+  if (permissions.every((result) => result.state === "granted")) {
+    console.log('success')
+    run();
+    setInterval(update, 250);
+  } else {
+    console.log("No permissions to use sensors.");
+  }
+};
+
+const update = async () => {
+  const request = await fetch(`https://${server}/angles`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({horizontal_angle: heading, vertical_angle: pitch})
+  })
+ 
+  const response = await request.json()
+};
+
+document.getElementById("start").addEventListener("click", setup)
